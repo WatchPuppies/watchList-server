5f59fbf27ef8b22f72f344b0cffe1812ad6fef38
JoviAndrew <jovian_andrew@hotmail.com>
2018-04-06 10:00:17 +0700

    remove add.js

--- routers/add.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const movies = require('../controllers/series');
-const anime = require('../controllers/anime');
-
-router.get('/search/movies/', movies.search_movies);
-router.get('/search/animes/', anime.search_anime);
-
-module.exports = router;
