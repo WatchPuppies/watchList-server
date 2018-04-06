c00fe17640787dc38d01fed3a3225f36e3aa47ad
haripermadi <harinurpermadi@gmail.com>
2018-04-06 11:19:11 +0700

    edit user

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const cors = require('cors')
 const loginRoute = require('./routers/login.js');
 const homeRoute = require('./routers/home.js');
 const showsRoute = require('./routers/show.js');
-
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
